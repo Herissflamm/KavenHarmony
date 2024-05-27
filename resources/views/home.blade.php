2d508bddb87b2fbda41b1f266c82b816a1d382a4
Adrien ROBERT <adri312678@outlook.fr>
2024-05-27 02:58:53 +0200

    Referrer Policy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,6 +21,8 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="referrer" content="origin-when-cross-origin" />
     @vite('resources/css/app.css')
 </head>
 <body>
